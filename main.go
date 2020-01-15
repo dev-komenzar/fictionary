eed0b6603c790ced1c7f3845569385610fe966a5
komenzar <takuya.kometan@gmail.com>
2020-01-15 22:55:25 +0900

    dbInsert()を削除。引数の受け渡しが難しかったため

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"strconv"
 	"time"
 
@@ -23,27 +22,6 @@ func dbInit() {
 	}
 	db.AutoMigrate(&data.Kaitou{}, &data.Game{})
 	defer db.Close()
-}
-
-//DBに追加
-func dbInsert(new interface{}, c *gin.Context) {
-	db, err := gorm.Open("postgres", connect)
-	if err != nil {
-		panic("データベース開ず(dbInsert)")
-	}
-	defer db.Close()
-
-	if db.NewRecord(new) == false {
-		panic("すでにデータが存在します。")
-	} else {
-		db.Create(&new)
-		if db.NewRecord(new) == false {
-			log.Println("History Recorded")
-		} else {
-			log.Println("History not created") //エラー内容を表示したい http://gorm.io/ja_JP/docs/error_handling.html
-			c.HTML(200, "error.html", gin.H{"message": "問題が作成されませんでした。もう一度試してください"})
-		}
-	}
 }
 
 //DBから一つ取り出す：回答ページで使用
@@ -68,7 +46,7 @@ func dbGetKaitou(id int) []data.Kaitou {
 	defer db.Close()
 
 	var kaitous []data.Kaitou
-	db.Where("GameID = ?", id).Find(kaitous)
+	db.Where("game_id = ?", id).Find(&kaitous)
 	return kaitous
 }
 
@@ -123,10 +101,18 @@ func main() {
 
 	router.POST("/newGame", func(c *gin.Context) {
 		text := c.PostForm("odai")
-		newGame := gameInit(text)
-		dbInsert(newGame, c) //DB：ゲームに登録
+		game := gameInit(text)
+		//dbInsert(game, c) //DB：ゲームに登録
 
-		id := strconv.Itoa(int(newGame.ID))
+		db, err := gorm.Open("postgres", connect)
+		if err != nil {
+			panic("データベース開ず(dbInsert)")
+		}
+		defer db.Close()
+
+		db.Create(&game)
+
+		id := strconv.Itoa(int(game.ID))
 		uri := "/games/" + id + "/new"
 		c.Redirect(302, uri)
 	})
@@ -157,7 +143,14 @@ func main() {
 		ans := c.PostForm("answer")
 
 		kaitou := makeAns(name, ans, iduint)
-		dbInsert(kaitou, c)
+		//INSERT
+		db, err := gorm.Open("postgres", connect)
+		if err != nil {
+			panic("データベース開ず(dbInsert)")
+		}
+		defer db.Close()
+
+		db.Create(&kaitou)
 
 		uri := "/games/" + n + "/accepted"
 		c.Redirect(302, uri)
@@ -174,7 +167,6 @@ func main() {
 
 	//「みんなの回答」ページを表示
 	router.GET("/games/:id", func(c *gin.Context) {
-		var kaitous []data.Kaitou
 		var numKaitou int
 
 		n := c.Param("id")
@@ -184,7 +176,7 @@ func main() {
 		}
 		game := dbGetOne(id)
 		answers := dbGetKaitou(id)
-		numKaitou = len(kaitous)
+		numKaitou = len(answers)
 		c.HTML(200, "phase3.html", gin.H{
 			"odai":         game.Odai,
 			"countOfUsers": numKaitou,
